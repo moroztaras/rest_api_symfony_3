e6dcda79e9455ce41340a37ee0bbc78b9504e389
moroztaras <moroztaras@i.ua>
2018-01-28 20:25:35 +0200

    Ra1 add controller for REST API

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -1,15 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: moroztaras
- * Date: 28.01.18
- * Time: 20:18
- */
-
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+use AppBundle\Entity\User;
 
-class UserController
+class UserController extends FOSRestController
 {
 
 }
