fe7acd9eb2e40a52c640b716ddd187a4d29d85d7
moroztaras <moroztaras@i.ua>
2018-01-31 19:11:08 +0200

    method for create post

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -5,9 +5,33 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\View\View;
-use AppBundle\Entity\User;
+use AppBundle\Entity\Post;
+use AppBundle\Form\PostType;
 
 class PostController extends FOSRestController
 {
+    /**
+     * Create a Post entity.
+     * @View(statusCode=201, serializerEnableMaxDepthChecks=true)
+     * @param Request $request
+     * @return Response
+     *
+     */
+    public function postAction(Request $request)
+    {
+        $entity = new Post();
+        $form = $this->createForm(new PostType(), $entity, array("method" => $request->getMethod()));
+        $this->removeExtraFields($request, $form);
+        $form->handleRequest($request);
 
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $entity;
+        }
+
+        return FOSView::create(array('errors' => $form->getErrors()), Codes::HTTP_INTERNAL_SERVER_ERROR);
+    }
 }
