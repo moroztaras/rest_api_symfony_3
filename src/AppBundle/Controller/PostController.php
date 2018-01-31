c0208f5fa60d4a513e29acedb1ddd83c845288e3
moroztaras <moroztaras@i.ua>
2018-01-31 19:06:18 +0200

    Rest Api for post

---
+++ src/AppBundle/Controller/PostController.php
@@ -0,0 +1,13 @@
+<?php
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+use AppBundle\Entity\User;
+
+class PostController extends FOSRestController
+{
+
+}
---
+++ src/AppBundle/Entity/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Post
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+}
